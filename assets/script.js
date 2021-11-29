36a89dd5b31e1a0ded9ee918b512f26b456faf73
Misty Whitenton <mistywhitenton@gmail.com>
2021-11-28 20:23:04 -0600

    Adjusted query selectors

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,12 @@
 const button = document.querySelector('.button')
-const inputValue = document.querySelector('.inputValue')
-const name = document.querySelector('.name')
-const desc = document.querySelector('.description')
-const temp = document.querySelector('.temperature')
+const currentCity = document.querySelector('#current-city')
 
 button.addEventListener('click', getApi);
 
 function getApi() {
-    let city = inputValue.value;
+    let city = currentCity.value;
     const APIKey = "2f710657032dbf024be6584bc8a359ee";
-    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
+    const queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
     fetch(queryURL) 
         .then(response => response.json())
         .then(data => console.log(data))
