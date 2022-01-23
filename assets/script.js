c49581ae7f51db2f06625b12d40f6558c7e848b2
Misty Whitenton <mistywhitenton@gmail.com>
2022-01-23 16:56:39 -0600

    Adding functionality to search form

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,60 @@
 var city;
 var searchForm = document.querySelector("#search-form");
-var searchBtn = document.querySelector("#search-button");
 var APIKey = "e256b571cb3fa1813dc8566aad393b43";
-var todayForecast = document.querySelector("#today-forecast");
-var todayDate = dayjs().format("MM/DD/YYYY");
-var fiveDayForecastEls = document.querySelectorAll(".day-forecast");
-var fiveDayDiv = document.querySelector("#five-day-forecast");
 var pastSearches = $("#past-searches");
-var searches = 0;
 var pastSearchesKeywords = [];
-var fiveDayDisplay = "";
+var searches = 0;
 
+// Clears out localstorage when the page loads
 localStorage.clear();
 
-// Search Form - Event Listener
+// Adds event listener to the search form submit button
 searchForm.addEventListener("submit", citySearch);
 
-// Search Form - Will Pass City Name to Weather API (Eventually)
+// Passes the user's city name from the search form to other functions
 function citySearch (event) {
     // Prevents form from being submitted
     event.preventDefault();
     // Assigns the user's search input to the "city" variable
-    var searchInput = document.querySelector("#city-search-input");
+    var searchInput = document.querySelector("#user-input");
     city = searchInput.value;
-    // If "city" variable exists, log it to the console for testing
+    // If "city" variable exists
+    if (city) {
+        // Saves searches to an array
+        pastSearchesKeywords.push(city);
+        // Adds searches array to localstorage as a string
+        localStorage.setItem("pastSearches", JSON.stringify(pastSearchesKeywords));
+        searches = searches + 1;
+        // Calls function to display past searches (see below)
+        displaySearches();
+    }
+}
+
+// Takes past searches from local storage & displays them as buttons to be clicked & searched again
+function displaySearches() {
+    // Clears out past searches
+    pastSearches.empty();
+    // Looks into localstorage for past searches
+    var searchHistory = JSON.parse(localStorage.pastSearches);
+    for (let i = 0; i < searchHistory.length; i++) {
+        var pastSearch = searchHistory[i];
+        // Creates buttons of past searches
+        var searchDisplay = document.createElement("button");
+        searchDisplay.textContent = pastSearch;
+        searchDisplay.setAttribute("class", "city-search-buttons");
+        searchDisplay.setAttribute("id", pastSearch);
+        // Logs the past searches to the console for testing
+        console.log(pastSearch);
+        pastSearches.append(searchDisplay);
+        // Searches the city again if button is clicked
+        searchDisplay.addEventListener("click", searchAgain);
+    }
+}
+
+function searchAgain(event) {
+    var city = event.target.getAttribute("id");
     if (city) {
         console.log(city);
+        // TODO: Pass city to OpenWeatherMap API
     }
 }
