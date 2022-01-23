6b52a84ca4ae607e63ab9a847869f070243b838c
Misty Whitenton <mistywhitenton@gmail.com>
2022-01-23 16:06:02 -0600

    Started working on saving searches to local storage

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,8 @@ const button = document.querySelector(".button")
 const currentCity = document.querySelector("#city-search-input");
 const apiKey = "2f710657032dbf024be6584bc8a359ee"; 
 
+localStorage.clear();
+
 function getApi() {
     var requestUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + currentCity + "&appid=" + apiKey;
     fetch(requestUrl) 
