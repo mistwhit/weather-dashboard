61f4908cbdc4ac040112c5bcf86b3fb63e118323
Misty Whitenton <mistywhitenton@gmail.com>
2022-01-23 16:21:30 -0600

    Tinkering with the search form

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,30 @@
-const button = document.querySelector(".button")
-const currentCity = document.querySelector("#city-search-input");
-const apiKey = "2f710657032dbf024be6584bc8a359ee"; 
+var city;
+var searchForm = document.querySelector("#search-form");
+var searchBtn = document.querySelector("#search-button");
+var APIKey = "e256b571cb3fa1813dc8566aad393b43";
+var todayForecast = document.querySelector("#today-forecast");
+var todayDate = dayjs().format("MM/DD/YYYY");
+var fiveDayForecastEls = document.querySelectorAll(".day-forecast");
+var fiveDayDiv = document.querySelector("#five-day-forecast");
+var pastSearches = $("#past-searches");
+var searches = 0;
+var pastSearchesKeywords = [];
+var fiveDayDisplay = "";
 
 localStorage.clear();
 
-function getApi() {
-    var requestUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + currentCity + "&appid=" + apiKey;
-    fetch(requestUrl) 
-        .then(function (response)  {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-        })
-        .catch(function (err) {
-            console.log(err);
-        })
-}
+// Search Form - Event Listener
+searchForm.addEventListener("submit", citySearch);
 
-button.addEventListener('click', getApi);
+// Search Form - Will Pass City Name to Weather API (Eventually)
+function citySearch (event) {
+    // Prevents form from being submitted
+    event.preventDefault();
+    // Assigns the user's search input to the "city" variable
+    var searchInput = document.querySelector("#city-search-input");
+    city = searchInput.value;
+    // If "city" variable exists, log it to the console for testing
+    if (city) {
+        console.log(city);
+    }
+}
