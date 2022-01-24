ae1549daf98122712153c85dd487e70da1223e23
Misty Whitenton <mistywhitenton@gmail.com>
2022-01-23 20:06:21 -0600

    Updated naming conventions for clarity

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,23 @@
 var city;
-var APIKey = "2f710657032dbf024be6584bc8a359ee";
+var apiKey = "2f710657032dbf024be6584bc8a359ee";
 var searchForm = document.querySelector("#search-form");
-var pastSearches = $("#past-searches");
-var pastSearchesKeywords = [];
+var previousSearches = $("#previous-searches");
+var previousSearchesArray = [];
 var searches = 0;
-var todayForecast = document.querySelector("#today-forecast");
-var todayDate = dayjs().format("MM/DD/YYYY");
-var fiveDayDiv = document.querySelector("#five-day-forecast");
+var todaysForecast = document.querySelector("#todays-forecast");
+var todaysDate = dayjs().format("MM/DD/YYYY");
+var fiveDayForecast = document.querySelector("#five-day-forecast");
 var fiveDayDisplay = "";
-var fiveDayForecastEls = document.querySelectorAll(".day-forecast");
+var fiveDayEls = document.querySelectorAll(".day-forecast");
 
 // Clears out localstorage when the page loads
 localStorage.clear();
 
 // Adds event listener to the search form submit button
-searchForm.addEventListener("submit", citySearch);
+searchForm.addEventListener("submit", searchByCity);
 
 // Passes the user's city name from the search form to other functions
-function citySearch (event) {
+function searchByCity (event) {
     // Prevents form from being submitted
     event.preventDefault();
     // Assigns the user's search input to the "city" variable
@@ -26,13 +26,13 @@ function citySearch (event) {
     // If "city" variable exists
     if (city) {
         // Calls function for today's forecast for searched city
-        forecastWeather(city);
+        todaysWeather(city);
         // Calls function for five day forecast for searched city
-        fiveDayForecast(city);
+        fiveDayWeather(city);
         // Saves searches to an array
-        pastSearchesKeywords.push(city);
+        previousSearchesArray.push(city);
         // Adds searches array to localstorage as a string
-        localStorage.setItem("pastSearches", JSON.stringify(pastSearchesKeywords));
+        localStorage.setItem("previousSearches", JSON.stringify(previousSearchesArray));
         searches = searches + 1;
         // Calls function to display past searches
         displaySearches();
@@ -40,11 +40,11 @@ function citySearch (event) {
 }
 
 // Takes past searches from local storage & displays them as buttons to be clicked & searched again
-function displaySearches() {
+function displaySearches () {
     // Clears out past searches
-    pastSearches.empty();
+    previousSearches.empty();
     // Looks into localstorage for past searches
-    var searchHistory = JSON.parse(localStorage.pastSearches);
+    var searchHistory = JSON.parse(localStorage.previousSearches);
     for (let i = 0; i < searchHistory.length; i++) {
         var pastSearch = searchHistory[i];
         // Creates buttons of past searches
@@ -54,35 +54,35 @@ function displaySearches() {
         searchDisplay.setAttribute("id", pastSearch);
         // Logs the past searches to the console for testing
         console.log(pastSearch);
-        pastSearches.append(searchDisplay);
+        previousSearches.append(searchDisplay);
         // Searches the city again if button is clicked
-        searchDisplay.addEventListener("click", searchAgain);
+        searchDisplay.addEventListener("click", storedSearch);
     }
 }
 
-// Returns the weather forecast for a city that was previously searched
-function searchAgain(event) {
+// Returns the weather forecast for a city that was previously searched and stored in localstorage
+function storedSearch (event) {
     var city = event.target.getAttribute("id");
     if (city) {
         console.log(city);
-        forecastWeather(city);
-        fiveDayForecast(city);
+        todaysWeather(city);
+        fiveDayWeather(city);
     }
 }
 
 // Today's Weather for Searched City
-function forecastWeather(city) {
+function todaysWeather (city) {
     // Creates query URL using city name and API Key
     var queryURL =
         "https://api.openweathermap.org/data/2.5/weather?q=" +
         city +
         "&units=imperial" +
         "&appid=" +
-        APIKey;
+        apiKey;
 
     // Clears out previous data 
-    todayForecast.textContent = "";
-    fiveDayForecastEls.textContent = "";
+    todaysForecast.textContent = "";
+    fiveDayEls.textContent = "";
     fiveDayDisplay.textContent = "";
 
     // Uses query URL to fetch data
@@ -93,8 +93,8 @@ function forecastWeather(city) {
 
         // City & Today's Date
         var dateDisplay = document.createElement("h2");
-        dateDisplay.textContent = city + " " + todayDate;
-        todayForecast.append(dateDisplay);
+        dateDisplay.textContent = city + " " + todaysDate;
+        todaysForecast.append(dateDisplay);
 
         // Icons
         var weatherIcon = data.weather[0].icon;
@@ -105,35 +105,35 @@ function forecastWeather(city) {
             "src",
             "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png"
         );
-        todayForecast.append(iconDisplay);
+        todaysForecast.append(iconDisplay);
 
         // Temperature
         var tempDisplay = document.createElement("li");
         tempDisplay.textContent = "Temp: " + data.main.temp + "°F";
-        todayForecast.append(tempDisplay);
+        todaysForecast.append(tempDisplay);
             
         // Wind Speed
         var windDisplay = document.createElement("li");
         windDisplay.textContent = "Wind: " + data.wind.speed + " MPH";
-        todayForecast.append(windDisplay);
+        todaysForecast.append(windDisplay);
 
         // Humidity
         var humidityDisplay = document.createElement("li");
         humidityDisplay.textContent = "Humidity: " + data.main.humidity + "%";
-        todayForecast.append(humidityDisplay);
+        todaysForecast.append(humidityDisplay);
 
         // Latitude and Longitude to get UV
         var lat = data.coord.lat;
         var lon = data.coord.lon;
         console.log(lat);
         console.log(lon);
-        getUV(lat, lon);
+        UV(lat, lon);
         });
     });
 }
 
 // Gets UV data and passes it back to today's weather forecast
-function getUV(lat, lon) {
+function UV (lat, lon) {
     // Creates query URL using latitude and longitude and API Key
     var queryURL =
         "https://api.openweathermap.org/data/2.5/onecall?lat=" +
@@ -141,7 +141,7 @@ function getUV(lat, lon) {
         "&lon=" +
         lon +
         "&appid=" +
-        APIKey;
+        apiKey;
 
     // Uses query URL to fetch data
     fetch(queryURL).then(function (response) {
@@ -164,21 +164,21 @@ function getUV(lat, lon) {
         }
 
         // Appends UV data to today's weather forecast for searched city
-        todayForecast.append(uvDisplay);
+        todaysForecast.append(uvDisplay);
         uvDisplay.append(uvDisplayNumber);
         });
     });
 }
 
 // Five Day Forecast for Searched City
-function fiveDayForecast(city) {
+function fiveDayWeather (city) {
     // Creates query URL using searched city and API Key
     var queryURL =
         "https://api.openweathermap.org/data/2.5/forecast?q=" +
         city +
         "&units=imperial" +
         "&appid=" +
-        APIKey;
+        apiKey;
 
     // Uses query URL to fetch data
     fetch(queryURL).then(function (response) {
@@ -188,11 +188,11 @@ function fiveDayForecast(city) {
         // Displays five day forecast heading
         fiveDayDisplay = document.createElement("h2");
         fiveDayDisplay.textContent = "5-Day Forecast";
-        fiveDayDiv.append(fiveDayDisplay);
+        fiveDayForecast.append(fiveDayDisplay);
 
         // Loops over html elements to display five day forecast
         for (let i = 0; i < 5; i++) {
-            fiveDayForecastEls[i].innerHTML = "";
+            fiveDayEls[i].innerHTML = "";
 
         // Gets data and displays dates for five day forecast
         var forecastDay = dayjs()
@@ -201,7 +201,7 @@ function fiveDayForecast(city) {
         console.log(forecastDay);
         var forecastDayDisplay = document.createElement("h3");
         forecastDayDisplay.textContent = forecastDay;
-        fiveDayForecastEls[i].append(forecastDayDisplay);
+        fiveDayEls[i].append(forecastDayDisplay);
 
         // Icons
         var weatherIcon = data.list[i].weather[0].icon;
@@ -212,23 +212,23 @@ function fiveDayForecast(city) {
             "src",
             "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png"
         );
-        fiveDayForecastEls[i].append(iconDisplay);
+        fiveDayEls[i].append(iconDisplay);
 
         // Temperature
         var tempDisplay = document.createElement("li");
         tempDisplay.textContent = "Temp: " + data.list[i].main.temp + "°F";
-        fiveDayForecastEls[i].append(tempDisplay);
+        fiveDayEls[i].append(tempDisplay);
 
         // Wind
         var windDisplay = document.createElement("li");
         windDisplay.textContent = "Wind: " + data.list[i].wind.speed + " MPH";
-        fiveDayForecastEls[i].append(windDisplay);
+        fiveDayEls[i].append(windDisplay);
 
         // Humidity
         var humidityDisplay = document.createElement("li");
         humidityDisplay.textContent =
         "Humidity: " + data.list[i].main.humidity + "%";
-        fiveDayForecastEls[i].append(humidityDisplay);
+        fiveDayEls[i].append(humidityDisplay);
         }
     });
 });
